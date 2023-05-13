2967a25e7a804407edc35d6fa11f7d30cac642ef
Conless <conlesspan@outlook.com>
2023-05-13 15:34:17 +0800

    feat: add some test files and edit .gitignore

---
+++ test/src/chinese_test.cpp
@@ -0,0 +1,13 @@
+/* UTF-8 Chinese character test
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;  // NOLINT
+
+auto main() -> int {
+  char s[100];
+  cin >> s;
+  cout << s << endl;
+  return 0;
+}
