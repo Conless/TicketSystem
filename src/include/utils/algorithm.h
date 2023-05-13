30c40b9db9040a2e08e288afd44588d49d7c83f1
Conless <conlesspan@outlook.com>
2023-05-14 01:24:32 +0800

    fix(src, script): edit run-test script, add a new sort().

--- src/include/utils/algorithm.h
+++ src/include/utils/algorithm.h
@@ -31,6 +31,31 @@ void sort(Iterator first, Iterator last, Compare comp) {  // NOLINT
   conless::sort(high + 1, last, comp);
 }
 
+template <typename Iterator>
+void sort(Iterator first, Iterator last) {  // NOLINT
+  auto low = first;
+  auto high = last - 1;
+  if (low - high >= 0) {
+    return;
+  }
+  auto temp = *first;
+  while (low != high) {
+    while (*high >= temp && high - low > 0) {
+      high--;
+    }
+    while (temp >= *low && high - low > 0) {
+      low++;
+    }
+    if (high - low > 0) {
+      std::swap(*low, *high);
+    }
+  }
+
+  std::swap(*first, *low);
+  conless::sort(first, low);
+  conless::sort(high + 1, last);
+}
+
 }  // namespace conless
 
 #endif
