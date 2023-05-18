f2fdc692149312341aa2e7764d5a612c57409ed5
Conless <conlesspan@outlook.com>
2023-05-18 23:06:09 +0800

    perf(train): edit buffer pool size to avoid mle.

--- src/train/train_system.cpp
+++ src/train/train_system.cpp
@@ -11,9 +11,9 @@ namespace conless {
 
 TrainSystem::TrainSystem(const std::string &file_name, bool inherit_file)
     : train_info_db_(file_name + "_train_info"),
-      train_date_info_db_(file_name + "_train_date_info", 20),
+      train_date_info_db_(file_name + "_train_date_info"),
       train_station_info_db_(file_name + "_train_station_info", 20),
-      ticket_info_db_(file_name + "_ticket_info", 55),
+      ticket_info_db_(file_name + "_ticket_info", 50),
       ticket_waitlist_info_db_(file_name + "_ticket_waitlist_info") {}
 
 auto TrainSystem::AddTrain(const TrainID &train_id, int station_num, int seat_num, const vector<std::string> &stations,
